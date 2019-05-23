f3a7ab8eb573eeb2f1c99be30ba526484fa6065b
scuwjw <hebeiweiwei@gmail.com>
2019-05-23 21:32:50 +0800

    Update and rename tmp_uniquepath.py to uniquepath.py

--- uniquepath.py
+++ uniquepath.py
@@ -1,6 +1,18 @@
-# -*- coding:utf-8 -*-
-
 class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m == 1 or n == 1:
+            return 1
+        dp = [[1 for i in range(n)] for j in range(m)]
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]
+        return dp[m-1][n-1]
+
     def uniquePathsWithObstacles(self, obstacleGrid):
         """
         :type obstacleGrid: List[List[int]]
@@ -8,22 +20,25 @@ class Solution(object):
         """
         m,n = len(obstacleGrid),len(obstacleGrid[0])
         dp = [[ 0 for i in range(n)] for j in range(m)]
-        for j in range(n):
+        for j in range(n-1,-1,-1):
             if obstacleGrid[m-1][j]:
                 break
             else:
                 dp[m-1][j] = 1
-        for i in range(m):
+        for i in range(m-1,-1,-1):
             if obstacleGrid[i][n-1]:
                 break
             else:
                 dp[i][n-1] = 1
+        print dp
         for i in range(m-2,-1,-1):
             for j in range(n-2,-1,-1):
-                if obstacleGrid[i][j] == 0:
+                if obstacleGrid[i][j] == 1:
                     dp[i][j] == 0
                 else:
                     dp[i][j] = dp[i+1][j] + dp[i][j+1]
         return dp[0][0]
+
 a = Solution()
-print a.uniqurPathsWithObstacles()    
+print a.uniquePaths(3,2)
+print a.uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]])
