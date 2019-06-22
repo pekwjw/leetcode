b413262b039d1e6fe5806b887b959e77f003c21d
scuwjw <hebeiweiwei@gmail.com>
2019-06-22 20:55:45 +0800

    Update unique_and_min_path.py

--- unique_and_min_path.py
+++ unique_and_min_path.py
@@ -57,6 +57,24 @@ class Solution(object):
                 dp[i][j] = min(dp[i-1][j],dp[i][j-1])+grid[i][j]
         return dp[m-1][n-1]
 
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix:
+            return 0
+        m, n = len(matrix),len(matrix[0])
+        dp = [[0 if matrix[j][i] == '0' else 1 for j in range(n)] for i in range(m)]
+        for i in range(1,m):
+            for j in range(1,n):
+                if matrix[i][j] == '1':
+                    dp[i][j] = min(dp[i-1][j],dp[i-1][j-1],dp[i][j-1])+1
+                else:
+                    dp[i][j] = 0
+        ans = max([max(i) for i in dp])
+        return ans**2
+
 a = Solution()
 print a.uniquePaths(7,3)
 print a.uniquePathsWithObstacles([[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0]])
