b314dde28687bf822d5614a50d1f6231a9d72624
scuwjw <hebeiweiwei@gmail.com>
2019-06-26 18:58:11 +0800

    Update and rename subarray_sum_product.py to subarray_sum_product_word.py

--- subarray_sum_product_word.py
+++ subarray_sum_product_word.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*- 
 
 class Solution(object):
-    def maxProduct(self, nums):
+    def maxProductNums(self, nums):
         """
         :type nums: List[int]
         :rtype: int
@@ -28,7 +28,25 @@ class Solution(object):
             maxnum = max(maxnum+tmp,tmp)
             ans = max(maxnum,ans)
         return ans
-    
+
+    def maxProduct(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        n = len(words)
+        elements = [0] * n
+        for i,tmp in enumerate(words):
+            for j in tmp:
+                elements[i] = elements[i] | (1 << ord(j) - ord('a'))
+        ans = 0
+        for i in range(n):
+            for j in range(i+1,n):
+                if elements[i] & elements[j] == 0:
+                    ans = max(ans,len(words[i])*len(words[j]))
+        return ans
+
 a = Solution()
-print a.maxProduct([2,3,-2,4])
+print a.maxProductNums([2,3,-2,4])
 print a.maxSubArray([-2,1,2,3,-7,8])
+print a.maxProduct(["abcw","baz","foo","bar","xtfn","abcdef"])
